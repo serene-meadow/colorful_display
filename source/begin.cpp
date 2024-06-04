9ce0c7811e403fe06ac0fa835d2095a959f60a89
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-06-04 14:35:55 -0500

    remove function stubs

--- source/begin.cpp
+++ source/begin.cpp
@@ -8,30 +8,8 @@
 
 #include "SdlContext.hpp"
 
-static void generateColorfulDisplay(Sint32 const width, Sint32 const height) {
-    (void)width;
-    (void)height;
-}
-
-static void drawOutwardColorGradient(double const centerX, double const centerY) {
-    namespace Sdl = SdlContext;
-
-    Sint32 const minLength{std::min(Sdl::windowWidth, Sdl::windowHeight)};
-    double const colorUnit = 360.0 / static_cast<double>(minLength);
-    
-    for (Sint32 y{0}; y < Sdl::windowHeight; ++y) {
-        for (Sint32 x{0}; x < Sdl::windowWidth; ++x) {
-            double const distance{std::sqrt(
-                std::pow(static_cast<double>(x) - centerX, 2.0) + std::pow(static_cast<double>(y) - centerY, 2.0)
-            )};
-
-
-        }
-    }
-}
-
 int main() {
-    namespace Sdl = SdlContext;
+    namespace Sdl = Project::SdlContext;
 
     Sdl::check(SDL_Init(SDL_INIT_VIDEO));
 
@@ -40,7 +18,7 @@ int main() {
     Sdl::window = Sdl::check(SDL_CreateWindow(
         "Colorful Display",
         SDL_WINDOWPOS_CENTERED/* x position */, SDL_WINDOWPOS_CENTERED/* y position */,
-        430/* width */, 430/* height */,
+        Sdl::getWindowWidth(), Sdl::getWindowHeight(),
         SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE
     ));
 
