286e8980d05ca135d9951ed9a2df00bfc2277d92
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-06-04 03:31:53 -0500

    Put all the SDL state and utility functions into one header.

---
+++ source/SdlContext.cpp
@@ -0,0 +1,59 @@
+#include "SdlContext.hpp"
+
+namespace SdlContext {
+    Uint64 deltaTime{0u};
+    SDL_Window *window = nullptr;
+    SDL_Renderer *renderer = nullptr;
+    Sint32 windowWidth{430};
+    Sint32 windowHeight{430};
+}
+
+void SdlContext::exitHandler() {
+    if (window != nullptr) SDL_DestroyWindow(window);
+    if (renderer != nullptr) SDL_DestroyRenderer(renderer);
+    SDL_Quit();
+}
+
+void SdlContext::mainLoop() {
+    // Time of the previous iteration.
+    static Uint64 previousTime{0u};
+
+    // Get the time of this iteration.
+    Uint64 const currentTime{SDL_GetTicks64()};
+
+    // Get the change in time.
+    SdlContext::deltaTime = currentTime - previousTime;
+
+    static SDL_Event event{};
+    while (SDL_PollEvent(&event)) switch (event.type) {
+        case SDL_KEYDOWN: switch (event.key.keysym.sym) {
+            case SDLK_BACKQUOTE:
+                SDL_SetWindowFullscreen(SdlContext::window, SDL_WINDOW_FULLSCREEN);
+                break;
+            case SDLK_ESCAPE:
+                SDL_SetWindowFullscreen(SdlContext::window, 0u);
+                break;
+        } break;
+        case SDL_WINDOWEVENT: switch (event.window.event) {
+            case SDL_WINDOWEVENT_RESIZED:
+                SdlContext::windowWidth = event.window.data1;
+                SdlContext::windowHeight = event.window.data2;
+                break;
+        } break;
+        case SDL_QUIT:
+            std::exit(EXIT_SUCCESS);
+            break;
+    }
+
+    refreshWindow();
+
+    // As this iteration ends, update the previous time.
+    previousTime = currentTime;
+
+    // Give the CPU a break.
+    SDL_Delay(1u);
+}
+
+void SdlContext::refreshWindow() {
+    ;;;
+}
---
+++ source/SdlContext.hpp
@@ -0,0 +1,41 @@
+#ifndef SdlContext_hpp
+#define SdlContext_hpp true
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wold-style-cast"
+#include "SDL.h"
+#pragma GCC diagnostic pop
+
+#include <iostream>
+
+namespace SdlContext {
+    [[noreturn]]
+    inline void errorOut() {
+        std::cerr << SDL_GetError() << '\n';
+        std::exit(EXIT_FAILURE);
+    }
+
+    inline constexpr void check(int const returnCode) {
+        if (returnCode != 0) errorOut();
+    }
+
+    template <typename PointerT>
+    inline constexpr PointerT check(PointerT const pointer) {
+        static_assert(std::is_pointer_v<PointerT>);
+        if (pointer == nullptr) errorOut();
+        else return pointer;
+    }
+
+    extern Uint64 deltaTime;
+    extern SDL_Window *window;
+    extern SDL_Renderer *renderer;
+    extern Sint32 windowWidth;
+    extern Sint32 windowHeight;
+
+    extern void exitHandler();
+    extern void mainLoop();
+    extern void refreshWindow();
+}
+
+
+#endif
